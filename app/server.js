d55ceacb723ba706586413bb25a3724f9d29a38f
Anders Ween <anders.ween@gmail.com>
2020-04-05 18:53:10 +0200

    add healthcheck

--- app/server.js
+++ app/server.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const express = require('express');
+const { HealthCheck } = require('@nrk/healthcheck');
 
 const PORT = 80;
 const HOST = '0.0.0.0';
+const health = new HealthCheck('pipeline-test', {});
 
 const app = express();
+
+app.get('/health', (req, res) => {
+  const report = health.report();
+  if (report.status === 'failed') {
+    res.status(500);
+  } else {
+    res.status(200);
+  }
+  res.send(health.report());
+}
+);
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
